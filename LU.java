9ef824cb79c4001cfb1fb4cfa5ec7a46f8b05fd0
César Alberto <cesaratj27@gmail.com>
2013-12-02 17:08:55 -0600

    Create LU.java

---
+++ LU.java
@@ -0,0 +1,46 @@
+/**
+*	LU.java
+*	Created on 22 November 2013, 11:13 hrs.
+*	A simple class wich uses LU method.
+*	@see Gauss.java
+*/
+
+/**
+*	@author: Equipo8
+*
+*			 -> Trejo Juárez César Alberto
+*			 -> Hernández Cuecuecha Jorge Alberto
+*			 -> Servín Lucario Verónica Valeria
+*			 -> José Alberto Martínez Lara
+*			 -> Fajardo Álvarez Samuel
+*/
+
+public class LU extends Gauss{
+
+	//Propiedades
+	public float [][]L;
+	public float [][]U;
+
+	//Constructor
+	public LU(int n){
+		super(n);
+		this.L = new float[n][n];
+		this.U = new float[n][n];
+	}
+
+	//Metodos
+
+	/** 
+	*	Metodo para obtener L
+	*	@return: array[n] 
+	*	@param: nothing
+	*/
+	public float[][] getL(){
+		int i,j,k;
+
+
+		
+
+		return L;
+	}
+}
